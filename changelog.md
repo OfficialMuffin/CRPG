9922c67a130b5591bb933a39f5ef6c8655733201
OfficialMuffin <OfficialMuffin@protonmail.com>
2018-09-24 15:36:27 +0100

    Updated Files
    Check Changelog

---
+++ changelog.md
@@ -0,0 +1,20 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+## 0.0.1 - 2018-09-24
+### Added
+- Author Name
+
+### Changed
+- Cleaned up code, separating potential repeated code into functions.
+
+### Removed
+- Character age as there wasn't a need to implement this
+
+### Known Issues
+- rand_items() does not generate a random item from the array
+
--- main.c
+++ main.c
@@ -1,15 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
-#define WELCOME_MSG "WELCOME TO MY RPG \n"
+#define WELCOME_MSG "WELCOME TO OFFICIALMUFFIN's RPG \n"
+#define RECIEVED_ITEM "***YOU HAVE RECIEVED AN ITEM!***\n"
+
+void rand_items() {
+
+    int max_inventory = 50;
+    int number_of_item;
+
+    char *items[3];
+    items[0] = "Plank of Wood";
+    items[1] = "Sheet of Metal";
+    items[2] = "Sharpening Stone";
+    items[3] = "Thick Rope";
+
+    char random; //2
+        srand(time(NULL));
+        random = items[rand() % 4];
+        printf(RECIEVED_ITEM);
+        printf("You Got ");
+        printf("%c", random);
+        number_of_item++;
+        max_inventory--;
+
+        return;
+
+}
+
+void rand_trash_items() {
+
+    int max_inventory = 50;
+    int number_of_trash_item;
+
+    char *trash_items[5];
+    trash_items[0] = "Piece of Paper with Scribbles";
+    trash_items[1] = "Just a Twig";
+    trash_items[2] = "Piece of Cloth";
+
+}
 
 int main()
 {
     printf(WELCOME_MSG);
 
-    //Initialize game variables
     char *char_name[25];
-    int char_age;
 
     printf("*You wake up in the middle of a deep forest, surrounded with small creatures that can talk your language* \n");
     Sleep(5000);
@@ -35,14 +70,23 @@ int main()
         printf("Glad you asked! We are in the woods, what more could you ask for :) \n");
         Sleep(2000);
         printf("*The creature laughs* Just joking! We are in the south-east of Woodheath, around three miles from the city centre.\n");
+        Sleep(2000);
+        printf("*He offers his hand to me* Come on! Lets get you back on your feet\n");
+        Sleep(2000);
+        printf("*As I get back on my feet again, the creature hands me something*\n");
+        Sleep(1000);
+        rand_items();
         break;
     case 2 :
         printf("Really now? Because you look like you've had a nasty fall.\n");
         Sleep(2000);
+        printf("Here, let me help you. *The creature offers me his hand*\n");
+        break;
+    default :
         break;
     }
 
-
+    printf("Breaking out of switch statement...");
 
 
     return 0;
